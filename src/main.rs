50242b7126a8df800336f81b396565b21b2aec88
Tobias Quinn <tobias@tobiasquinn.com>
2023-05-28 13:03:04 +0100

    reads the archlinux version and notifies if different

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use ntfy::{Dispatcher, Payload, Priority};
 use reqwest::blocking::Client;
+use std::process::Command;
+
+fn get_archlinux_version(package_name: &str) -> String {
+    let output = Command::new("pacman")
+        .arg("-Qi")
+        .arg(package_name)
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let lines: Vec<&str> = stdout.split('\n').collect();
+    let version_line = lines
+        .iter()
+        .find(|line| line.starts_with("Version"))
+        .unwrap();
+    let version = version_line.split(": ").nth(1).unwrap().to_string();
+    let main_version = version.split("-").nth(0).unwrap().to_string();
+
+    main_version
+}
 
 fn get_latest_version() -> String {
     let client = Client::new();
@@ -34,13 +54,18 @@ fn get_latest_version() -> String {
 const NTFY_CHANNEL: &str = "dcecef08-1839-40ed-b0ed-eee980594295";
 
 fn main() {
-    println!("Hello, world!");
     let version = get_latest_version();
-    println!("Latest: {}", version);
-    let dispatcher = Dispatcher::builder("https://ntfy.sh").build().unwrap();
-    let payload = Payload::new(NTFY_CHANNEL)
-        .priority(Priority::High)
-        .message(format!("Latest insiders version is {}", version))
-        .title("VSCode Update");
-    dispatcher.send(&payload).unwrap();
+    println!("Published latest: {}", version);
+    let archlinux_version = get_archlinux_version("visual-studio-code-insiders-bin");
+
+    if version != archlinux_version {
+        let dispatcher = Dispatcher::builder("https://ntfy.sh").build().unwrap();
+        let payload = Payload::new(NTFY_CHANNEL)
+            .priority(Priority::High)
+            .message(format!("New insiders version available {}", version))
+            .title("VSCode Update");
+        dispatcher.send(&payload).unwrap();
+    } else {
+        println!("No new version available");
+    }
 }
